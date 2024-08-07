8b5bfd12f306a88c0a41522d03013ad6202cface
Danial <win303065@gmail.com>
2024-08-07 13:26:10 +0900

    Add test codes

---
+++ packages/client-test/scripts/11-history/shop.ts
@@ -0,0 +1,38 @@
+import { HTTPClient } from "../../src/HttpClient";
+import { Helper } from "../utils";
+import URI from "urijs";
+import { ContextBuilder, ContextParams } from "acc-sdk-client-v2";
+
+const beautify = require("beautify");
+
+async function main() {
+    const contextParams: ContextParams = ContextBuilder.buildContextParams(Helper.NETWORK, Helper.TEST_PK);
+    if (Helper.RELAY_ENDPOINT !== "") contextParams.relayEndpoint = Helper.RELAY_ENDPOINT;
+    if (Helper.WEB3_ENDPOINT !== "") contextParams.web3Provider = Helper.WEB3_ENDPOINT;
+    const shopInfo = Helper.loadShopInfo();
+
+    const client = new HTTPClient();
+    const url = URI(contextParams.relayEndpoint)
+        .directory("/v1/shop/history")
+        .filename(shopInfo.shopId)
+        .addQuery("pageNumber", 1)
+        .addQuery("pageSize", 100)
+        .addQuery("actions", "1,2,3")
+        .toString();
+    const response = await client.get(url);
+    if (response.data.code !== 0) {
+        console.error("Error!", response.data.error.message);
+        process.exit(response.data.code);
+    }
+
+    console.log("처리결과입니다.");
+    console.log(response.data.code);
+    console.log(beautify(JSON.stringify(response.data.data), { format: "json" }));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
--- packages/client-test/scripts/3-shop/createId.ts
+++ packages/client-test/scripts/3-shop/createId.ts
@@ -1,9 +1,18 @@
 import { ContractUtils, LoyaltyNetworkID } from "acc-sdk-client-v2";
 import { Wallet } from "ethers";
+import { Helper } from "../utils";
 
 async function main() {
     const wallet = Wallet.createRandom();
-    const shopId = ContractUtils.getShopId(wallet.address, LoyaltyNetworkID.ACC_TESTNET);
+    let shopId = "";
+
+    if (Helper.NETWORK === "acc_mainnet") {
+        ContractUtils.getShopId(wallet.address, LoyaltyNetworkID.ACC_MAINNET);
+    } else if (Helper.NETWORK === "acc_testnet") {
+        ContractUtils.getShopId(wallet.address, LoyaltyNetworkID.ACC_TESTNET);
+    } else {
+        ContractUtils.getShopId(wallet.address, LoyaltyNetworkID.ACC_TESTNET);
+    }
 
     console.log("처리결과입니다.");
     console.log(`shopId: ${shopId}`);
